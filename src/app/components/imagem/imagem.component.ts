497b193b71058168c0d7457305bc3ff66aeb8905
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-02-25 15:24:34 -0300

    chore: refactor to better adhere to the requested specification

--- src/app/components/imagem/imagem.component.ts
+++ src/app/components/imagem/imagem.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { opendir } from 'node:fs';
 
 @Component({
   selector: 'app-imagem',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './imagem.component.scss'
 })
 export class ImagemComponent {
-  biscoito: string = '/biscoito.png'
-  biscoitoAberto: string = '/biscoito-aberto.png'
+  cookieImage: string = "/biscoito-aberto.png"
+
+  public swapCookie() : void
+  {
+    const opened = "/biscoito-aberto.png"
+    const closed = "/biscoito.png"
+
+    this.cookieImage = (this.cookieImage == opened) ? closed : opened;
+  }
 }
