cd1e7f589a4238326a3219f04d1655023aa0a59a
jpaveiro <joaopedroaveiro2016@gmail.com>
2025-02-25 15:31:47 -0300

    feat: frases component

---
+++ src/app/components/frases/frases.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frases',
+  imports: [],
+  templateUrl: './frases.component.html',
+  styleUrl: './frases.component.scss'
+})
+export class FrasesComponent {
+  phrases: string[] = ["O café está quente.", "Preciso dormir cedo.", "O sol nasceu forte hoje.",
+    "Esqueci minha senha de novo.", "A música está muito alta.", "Quero viajar neste fim de semana.",
+    "O cachorro latiu a noite toda.", "A bateria do celular acabou rápido.", "Choveu bastante ontem.",
+    "O tempo passou voando."]
+  showMessage: boolean = true;
+  drawnPhrase: string = ""
+
+  public randomPhrase() : void {    
+    this.showMessage = !this.showMessage;
+
+    if (this.showMessage || this.drawnPhrase == "")
+    {
+      this.drawnPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)]
+    }
+  }
+}
+
